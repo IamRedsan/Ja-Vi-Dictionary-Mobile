da2f5368738c2a5a975897f0719326a311e82615
ino_ermes <hikigayahachiman.akame.kill@gmail.com>
2024-11-17 02:49:14 +0700

    upgrade to sdk 52, add text to speech

--- components/translate/Source.tsx
+++ components/translate/Source.tsx
@@ -3,6 +3,8 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import TextArea from '../ui/TextArea';
 import { cssInterop } from 'nativewind';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as Speech from 'expo-speech';
 
 cssInterop(AntDesign, {
   className: {
@@ -13,6 +15,7 @@ cssInterop(AntDesign, {
 
 interface SourceProps {
   title: string;
+  language: 'ja-JP' | 'vi-VN';
   text?: string;
   onChangeText?: (text: string) => void;
   disabled?: boolean;
@@ -23,13 +26,23 @@ const Source: React.FC<SourceProps> = ({
   disabled,
   text,
   onChangeText,
+  language,
 }) => {
+  const handleSpeak = () => {
+    if (!text || text.length === 0) {
+      return;
+    }
+    Speech.stop();
+    Speech.speak(text, { language });
+  };
+
   return (
     <View className='p-4'>
       <View className='flex-row justify-between items-center'>
         <Text className='text-text text-lg'>{title}</Text>
         <TouchableOpacity
           disabled={disabled}
+          className='p-1'
           onPress={() => {
             if (onChangeText) onChangeText('');
           }}>
@@ -49,6 +62,19 @@ const Source: React.FC<SourceProps> = ({
           disabled ? 'text-gray-500' : 'text-text'
         } caret-primary min-h-[200px]`}
       />
+      <View className='flex-row justify-end items-center'>
+        <TouchableOpacity
+          className='p-1'
+          disabled={disabled}
+          onPress={handleSpeak}>
+          <FontAwesome
+            name='volume-up'
+            className={`${
+              disabled ? 'text-gray-500' : 'text-text'
+            } text-[20px]`}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- components/translate/Target.tsx
+++ components/translate/Target.tsx
@@ -4,6 +4,8 @@ import TextArea from '../ui/TextArea';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { cssInterop } from 'nativewind';
 import * as Clipboard from 'expo-clipboard';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as Speech from 'expo-speech';
 
 cssInterop(FontAwesome6, {
   className: {
@@ -15,12 +17,21 @@ cssInterop(FontAwesome6, {
 interface TargetProps {
   title: string;
   text?: string;
+  language: 'ja-JP' | 'vi-VN';
   loading?: boolean;
 }
 
-const Target: React.FC<TargetProps> = ({ title, text, loading }) => {
-  const copyToClipboard = async () => {
+const Target: React.FC<TargetProps> = ({ title, text, loading, language }) => {
+  const handleCopyToClipboard = async () => {
     if (text) await Clipboard.setStringAsync(text);
+  };
+
+  const handleSpeak = () => {
+    if (!text || text.length === 0) {
+      return;
+    }
+    Speech.stop();
+    Speech.speak(text, { language });
   };
 
   return (
@@ -34,9 +45,22 @@ const Target: React.FC<TargetProps> = ({ title, text, loading }) => {
         value={text}
         editable={false}
       />
-      <View className='flex-row justify-between'>
-        <View />
-        <TouchableOpacity disabled={loading} onPress={copyToClipboard}>
+      <View className='flex-row justify-end gap-4'>
+        <TouchableOpacity
+          className='p-1'
+          disabled={loading}
+          onPress={handleSpeak}>
+          <FontAwesome
+            name='volume-up'
+            className={`${
+              loading ? 'text-gray-500' : 'text-white'
+            } text-[20px]`}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          className='p-1'
+          disabled={loading}
+          onPress={handleCopyToClipboard}>
           <FontAwesome6
             name='copy'
             className={`text-[20px] ${
